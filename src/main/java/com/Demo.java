9e0fea5ff25a2219d04419d9bda5f728bdae158f
Sv77 <rimma34@gmail.com>
2020-06-15 00:36:01 +0200

    s8 mathe

--- src/main/java/com/Demo.java
+++ src/main/java/com/Demo.java
@@ -4,10 +4,15 @@ public class Demo {
 
 	public static void main(String[] args) {
 		for (int i = 0; i < 5; i++) {
-			System.out.println("frosh");	
-			System.out.println("ich will trinken");	
-			System.out.println("ich will tanzen");	
+			System.out.println("frosh");
+			System.out.println("ich will trinken");
+			System.out.println("ich will tanzen");
 		}
+
+		int a = 5;
+		int b = 41;
+		int res = a + b;
+		System.out.println(res);
 	}
 
 }

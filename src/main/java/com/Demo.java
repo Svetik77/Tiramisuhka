0017d6ff4b0c3224f02450e3c897ad7b25924604
Sv77 <rimma34@gmail.com>
2020-06-15 00:23:15 +0200

    s6 changed prints

--- src/main/java/com/Demo.java
+++ src/main/java/com/Demo.java
@@ -4,7 +4,7 @@ public class Demo {
 
 	public static void main(String[] args) {
 		for (int i = 0; i < 5; i++) {
-			System.out.println("ich will essen");	
+			System.out.println("frosh");	
 		}
 	}
 

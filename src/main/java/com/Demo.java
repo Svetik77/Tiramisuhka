c34cdf31a1df956749d6d55eea6a825cae7e22a8
Sv77 <rimma34@gmail.com>
2020-06-15 00:27:20 +0200

    solved conflict

--- src/main/java/com/Demo.java
+++ src/main/java/com/Demo.java
@@ -4,11 +4,8 @@ public class Demo {
 
 	public static void main(String[] args) {
 		for (int i = 0; i < 5; i++) {
-<<<<<<< HEAD
 			System.out.println("frosh");	
-=======
 			System.out.println("ich will trinken");	
->>>>>>> branch 'master' of git@github.com:Svetik77/Tiramisuhka.git
 		}
 	}
 
